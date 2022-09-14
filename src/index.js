e72e4208eff2044d8afa408adbfa70927a655954
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 01:32:22 +0300

    feat(reactium)  : router created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './style/style.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Todos } from 'pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/todos' element={<Todos />} />
+    </Routes>
+  </BrowserRouter>
+);
