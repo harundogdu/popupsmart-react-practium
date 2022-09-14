de9752f5115360e1334a28adce94437534be3031
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 01:31:50 +0300

    feat(reactium)  : home page created and designed start

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className='home'>
+      <div className='home__container'>
+        <h1 className='home__container-title'>Welcome to Todo App</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
