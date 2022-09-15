cd593b2f6463d6698c850cf6eac40dc5d21ea75a
Harun Doğdu <harundogdu06@gmail.com>
2022-09-16 00:37:35 +0300

    feat(practium) : private and public routes created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/style.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home, Todos } from 'pages';
 import { ThemeProvider } from 'contexts/theme-context';
 import { AuthProvider } from 'contexts/auth-context';
 import { TodoProvider } from 'contexts/todo-context';
+
+const user = JSON.parse(localStorage.getItem('username'));
+
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to='/' replace />;
+  }
+
+  return children;
+};
+
+const HasUserRoute = ({ user, children }) => {
+  if (user) {
+    return <Navigate to='/todos' replace />;
+  }
+
+  return children;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +32,23 @@ root.render(
       <TodoProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/todos' element={<Todos />} />
+            <Route
+              path='/'
+              element={
+                <HasUserRoute user={user}>
+                  <Home />
+                </HasUserRoute>
+              }
+            />
+            <Route
+              path='/todos'
+              element={
+                <ProtectedRoute user={user}>
+                  <Todos />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='*' element={<div>404</div>} />
           </Routes>
         </BrowserRouter>
       </TodoProvider>
