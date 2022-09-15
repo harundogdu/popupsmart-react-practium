8b03d7ed3770287012202094def338512ea6bfa8
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 17:51:48 +0300

    ui(practium) : some modal parameters changed

--- src/components/shared/modal/index.jsx
+++ src/components/shared/modal/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const CommonModal = ({ title, children, onClick }) => {
+const CommonModal = ({ title, children, onClick, setVisible,buttonText }) => {
   return (
     <div className='modal'>
       <div className='modal__container'>
         <div className='modal__container-header'>
           <h2 className='modal__container-header-title'>{title}</h2>
-          <button className='modal__container-header-close'>
-            <i className='fas fa-times'></i>
+          <button
+            className='modal__container-header-close'
+            onClick={() => setVisible(false)}
+          >
+            X
           </button>
         </div>
         <div className='modal__container-body'>{children}</div>
         <div className='modal__container-footer'>
           <button className='modal__container-footer-button' onClick={onClick}>
-            Save
+            {buttonText}
           </button>
         </div>
       </div>
