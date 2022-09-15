13a14343dcdf9c1d1ccb319427f6d56b9ed47c5e
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 15:09:39 +0300

    feat(practium) : contexts used to index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './style/style.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Todos } from 'pages';
+import { ThemeProvider } from 'contexts/theme-context';
+import { AuthProvider } from 'contexts/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/todos' element={<Todos />} />
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/todos' element={<Todos />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </ThemeProvider>
 );
