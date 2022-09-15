7912f8493b512bf957cbe9e517800d97d86122c3
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 17:52:35 +0300

    feat(practium) : add todo operations done

--- src/containers/todo-header/index.jsx
+++ src/containers/todo-header/index.jsx
@@ -1,26 +1,57 @@
-function TodoHeader({ type }) {
+import { useTodo } from 'contexts/todo-context';
+import { useState } from 'react';
+
+function TodoHeader() {
+  const { addTodo, loading, todos } = useTodo();
+  const [activeTodo, setActiveTodo] = useState({
+    content: '',
+    isCompleted: false
+  });
+
+  const handleAddTodoButtonClick = e => {
+    e.preventDefault();
+
+    if (activeTodo.content.length <= 3) {
+      alert('Please make sure todo is at least 3 characters long');
+      return;
+    }
+
+    if (todos.find(item => item.content === activeTodo.content)) {
+      alert('Todo already exists');
+      return;
+    }
+
+    setActiveTodo({ content: '', isCompleted: false });
+    addTodo(activeTodo);
+  };
+
   return (
     <section className='todo-header'>
       <div className='todo-header__card'>
         <div className='todo-header__card-header'>
-          {type === 'add' ? <h1>Add Todo</h1> : <h1>Update Todo</h1>}
+          <h1>Add Todo</h1>
         </div>
         <div className='todo-header__card-body'>
-          <form action=''>
+          <form onSubmit={handleAddTodoButtonClick}>
             <div className='form-group'>
               <input
                 type='text'
                 placeholder='enter ur todo...'
                 required
                 min={3}
+                value={activeTodo.content}
+                onChange={e => setActiveTodo({ content: e.target.value })}
               />
             </div>
             <div className='button-area'>
-              {type === 'add' ? (
-                <button className='btnAdd'>Add</button>
-              ) : (
-                <button className='btnUpdate'>Update</button>
-              )}
+              <button
+                type='submit'
+                className='btnAdd'
+                onClick={handleAddTodoButtonClick}
+                disabled={loading}
+              >
+                Add
+              </button>
             </div>
           </form>
         </div>
