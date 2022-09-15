eacc06e3c39b30461c328d4fbf6cf9d248568d18
Harun Doğdu <harundogdu06@gmail.com>
2022-09-15 15:09:26 +0300

    feat(practium) : theme-context created

---
+++ src/contexts/theme-context.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const initialState = {
+  theme: JSON.parse(localStorage.getItem('theme')) || 'light'
+};
+
+const ThemeContext = createContext(initialState);
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(initialState.theme);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', JSON.stringify(newTheme));
+  };
+
+  const value = {
+    theme,
+    toggleTheme
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => useContext(ThemeContext);
+
+export { ThemeProvider, useTheme };
