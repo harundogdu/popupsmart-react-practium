63d7cd33c799c98bd75e120da5e1331800668ab3
Harun Doğdu <harundogdu06@gmail.com>
2022-09-16 01:36:03 +0300

    fix(practium) : navbar error fixed

--- src/containers/navbar/index.jsx
+++ src/containers/navbar/index.jsx
@@ -1,10 +1,18 @@
 import { useAuth } from 'contexts/auth-context';
 import { useTheme } from 'contexts/theme-context';
+import { useNavigate } from 'react-router-dom';
 import { ToggleButton } from './components';
 
 function Navbar() {
+  const navigate = useNavigate();
   const { theme, toggleTheme } = useTheme();
-  const { username } = useAuth();
+  const { username, setAuth, setUserName } = useAuth();
+
+  const handleLogoutClick = () => {
+    setAuth(false);
+    setUserName('');
+    navigate('/');
+  };
 
   return (
     <nav className='navbar'>
@@ -13,6 +21,9 @@ function Navbar() {
         <ul className='navbar__container-menu'>
           <li>
             <span>{username}</span>
+          </li>
+          <li>
+            <button className='btnLogout' onClick={handleLogoutClick}>Logout</button>
           </li>
           <li>
             <ToggleButton name='theme' onChange={toggleTheme} value={theme} />
